f84d5b2eb1180469b759265f99fc656cedc099dd
ratih shinta <rstihshintaaa6@gmail.com>
2023-11-20 12:58:16 +0700

    PAS 20-11-2023

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\studentController;
 use App\Models\student;
 use App\Http\Controllers\productController;
 use App\Models\product;
+use App\Http\Controllers\replacementController;
+use App\Models\replacement;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +28,6 @@ Route::get('/student/detail/{student}', [studentController::class, 'show']);
 
 Route::get('/product/all', [productController::class, 'index']);
 Route::get('/product/detail/{product}', [productController::class, 'show']);
+
+Route::get('/replacement/all', [replacementController::class, 'index']);
+Route::get('/replacement/detail/{replacement}', [replacementController::class, 'show']);
